7b4d68526c16f7caba3a231eee537f029fbadfdf
Aishwarya Jadhav <72146224+AishwaryaJadhav777@users.noreply.github.com>
2020-11-06 23:13:41 +0530

    Add files via upload

---
+++ Nov_6_AngularProject/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Welcome to Angular';
+    title1 = 'Here we will Practice all Typescript Logics';
+    subtitle = 'Programming is Fun';
+    subtitle1 = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';
+
+    //declare the array
+    numbers: number[] = [10, 20, 30, 40];
+
+    //number is a variable//Number name of the array
+
+    
+}
